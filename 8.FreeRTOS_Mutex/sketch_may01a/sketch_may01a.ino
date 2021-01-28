dcbe8e2d01222038c06e6daa70504c5f9f143830
avinaash67 <43965193+avinaash67@users.noreply.github.com>
2021-01-28 02:03:31 +0100

    Update sketch_may01a.ino

--- 8.FreeRTOS_Mutex/sketch_may01a/sketch_may01a.ino
+++ 8.FreeRTOS_Mutex/sketch_may01a/sketch_may01a.ino
@@ -1,32 +1,8 @@
-//*****************************************************************************************************************************************************************************************
-//***************************************************************************  Arduino Simulator Drag & Draw  *****************************************************************************
-//*******************************************************************  Copyright by Marc Van Den Berge and Louis D'hont  ******************************************************************
-//*****************************************************************************************************************************************************************************************
 
 #include "SimulatorProgram.h"
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
-/*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13. 
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead(). 
- 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
- 
- * Note: because most Arduinos have a built-in LED attached 
- to pin 13 on the board, the LED is optional.
- 
- http://arduino.cc/en/Tutorial/AnalogInput
- 
- */
+
 //Create Tasks
 void Task_ControlLed1(void *param);
 void Task_ControlLed2(void *param);
